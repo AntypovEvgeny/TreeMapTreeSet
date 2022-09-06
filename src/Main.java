566ed06b04b3eded042d6791c266b6a13e4d93b1
Evgeny Antipov <e.antipov@gmail.com>
2022-09-06 17:54:51 +0300

    delete Class, add lambda

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,16 @@ public class Main {
         people.add(new Person("Tolya", "Muhov", 21));
         people.add(new Person("Sveta", "Vegan Mari", 18));
 
-        Collections.sort(people, new PersonsSurnameComparator(2));
+        people.sort((o1, o2) -> {
+            int quantityWordSurname1 = o1.getSurname().split(" +").length;
+            int quantityWordSurname2 = o2.getSurname().split(" +").length;
+
+            if (quantityWordSurname1 == quantityWordSurname2 || quantityWordSurname1 >= 2) {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            } else {
+                return Integer.compare(quantityWordSurname1, quantityWordSurname2);
+            }
+        });
         System.out.println(people);
     }
 }
