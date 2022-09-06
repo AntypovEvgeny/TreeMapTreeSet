347e754f7908b09eb09ad244b1625309783255f4
Evgeny Antipov <e.antipov@gmail.com>
2022-09-06 17:21:42 +0300

    Заменен объект удалённого класса на лямбду с той же логикой

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,16 @@ public class Main {
         people.add(new Person("Foma", "Ivlev Lobov Znatokov", 18));
         people.add(new Person("Tolya", "Muhov", 21));
 
-        Collections.sort(people, new PersonsSurnameComparator(2));
+        people.sort((o1, o2) -> {
+            int quantityWordSurname1 = o1.getSurname().split(" +").length;
+            int quantityWordSurname2 = o2.getSurname().split(" +").length;
+
+            if (quantityWordSurname1 == quantityWordSurname2 || quantityWordSurname1 >= 2) {
+                return Integer.compare(o1.getAge(), o2.getAge());
+            } else {
+                return Integer.compare(quantityWordSurname1, quantityWordSurname2);
+            }
+        });
         System.out.println(people);
     }
 }
